47f6d0f445360d8b59fc113554e85e8aebcecba3
mihnsen <minhnt.hut@gmail.com>
2022-02-19 14:27:05 +0700

    Basic component

---
+++ src/components/Ecosystem.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Multiselect from 'vue-multiselect'
+import { ref } from 'vue'
+
+const count = ref(1)
+const options = ref(['list', 'of', 'options'])
+const value = []
+</script>
+<template>
+  <div class="ecosystem">
+    <h2> Ecosystem </h2>
+    <pre style="color: #fff">
+      {{ count }}
+      {{ options }}
+      {{ value }}
+    </pre>
+    <multiselect v-model="value" :options="options" :multiple="true"></multiselect>
+  </div>
+</template>
+
+<style src="vue-multiselect/dist/vue-multiselect.css"></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import Ecosystem from '@/components/Ecosystem.vue'
 import TheWelcome from '@/components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
+    <Ecosystem />
     <TheWelcome />
   </main>
 </template>
