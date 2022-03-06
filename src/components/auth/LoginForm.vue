c3d3ad290c8fd46d1e0b01383deabe73bf2c2276
mihnsen <minhnt.hut@gmail.com>
2022-03-06 17:49:40 +0700

    Update: add pure login view

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,6 +1,50 @@
 <template lang="pug">
-h2 Login
+div
+  h2 Login
+  //Form(@submit="submit")
+  //FormLayout
+    // TODO TextField
+  form.login-form(
+    novalidate,
+    method="post",
+    @submit.prevent="submit"
+  )
+    input(
+      type="text",
+      placeholder="you_shopify_domain.myshopify.com",
+      v-model="shop"
+    )
+    button(type="submit")
+      span.mr-2(
+        v-if="isProcessing.value",
+      ) loading..
+      span Submit
 </template>
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
+import { Spinner } from '@qikify/polaris-vue';
+
+// Ref
+const isProcessing = ref(false);
+
+// React
+const loginData = {
+  shop: '',
+};
+
+/**
+* Methods
+*/
+const submit = (): void => {
+  // TODO
+  doLogin();
+};
+
+const doLogin = (): void => {
+  isProcessing.value = true;
+  setTimeout(() => {
+    console.log('login success');
+    isProcessing.value = false;
+  }, 2000);
+};
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3601
+    port: 3600
   },
   plugins: [vue()],
   resolve: {
