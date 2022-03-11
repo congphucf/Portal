95da7919985afc02c9f8d09374bed39bfbc2a169
mihnsen <minhnt.hut@gmail.com>
2022-03-11 15:03:53 +0700

    Update: cleanup unuse components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
-import Ecosystem from '@/components/Ecosystem.vue'
-import TheWelcome from '@/components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <Ecosystem />
-    <TheWelcome />
   </main>
 </template>
