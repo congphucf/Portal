1f467970b7a241249c44ce70ba746500bfaa3567
mihnsen <minhnt.hut@gmail.com>
2022-04-06 12:05:56 +0700

    Update: add vue airbnb eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,40 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier"
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    // 'eslint:recommended',
+    // '@vue/eslint-config-typescript/recommended',
+    // '@vue/eslint-config-prettier'
+    '@vue/airbnb',
+    '@vue/typescript/recommended',
   ],
-  "env": {
-    "vue/setup-compiler-macros": true
+  env: {
+    'vue/setup-compiler-macros': true,
   },
-  "overrides": [
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/airbnb',
+    '@vue/typescript/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'object-curly-newline': 'off',
+  },
+  overrides: [
     {
-      "files": [
-        "cypress/integration/**.spec.{js,ts,jsx,tsx}"
+      files: [
+        'cypress/integration/**.spec.{js,ts,jsx,tsx}',
       ],
-      "extends": [
-        "plugin:cypress/recommended"
-      ]
-    }
-  ]
-}
+      extends: [
+        'plugin:cypress/recommended',
+      ],
+    },
+  ],
+};
